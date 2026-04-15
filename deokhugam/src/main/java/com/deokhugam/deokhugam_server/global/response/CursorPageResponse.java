4490adccd28fe0704c222ac63db61c2710854be7
minjukoo <168660759+minjukoo@users.noreply.github.com>
2026-04-15 11:35:07 +0900

    Feat/global common (#3)
    커서페이지 관련 코드 작성했습니다. CursorPageUtil, CursorPageResponse

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/global/response/CursorPageResponse.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/global/response/CursorPageResponse.java
@@ -1,5 +1,13 @@
 package com.deokhugam.deokhugam_server.global.response;
 
-public class CursorPageResponse {
+import java.time.LocalDateTime;
+import java.util.List;
 
-}
+public record CursorPageResponse<T>(
+    List<T> content,        // 실제 데이터 리스트
+    String nextCursor,      // 다음 조회를 위한 커서 ID
+    LocalDateTime nextAfter, // 다음 조회를 위한 시간 기준점
+    int size,               // 현재 페이지에 담긴 개수
+    long totalElements,     // 전체 데이터 개수
+    boolean hasNext         // 다음 페이지가 더 있는지 여부
+) {}
