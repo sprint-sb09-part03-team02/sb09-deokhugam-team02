1088c14a9ceaeeeeedbcc849d6b193c9b6c39858
구민주 <mj55991188@gmail.com>
2026-04-16 11:01:54 +0900

    feat: 엔티티-DTO 변환을 위한 CommentMapper 구현

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/comment/mapper/CommentMapper.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/comment/mapper/CommentMapper.java
@@ -1,4 +1,13 @@
 package com.deokhugam.deokhugam_server.domain.comment.mapper;
 
-public class CommentMapper {
+import com.deokhugam.deokhugam_server.domain.comment.dto.response.CommentDto;
+import com.deokhugam.deokhugam_server.domain.comment.entity.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+  @Mapping(target = "userNickname", source = "userNickname")
+  CommentDto toDto(Comment comment, String userNickname);
 }
