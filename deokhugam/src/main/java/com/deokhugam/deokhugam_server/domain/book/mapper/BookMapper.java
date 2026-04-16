1f87039a437cfaf21c22708785c99e3a73256884
sungho <135408197+parksh3070@users.noreply.github.com>
2026-04-16 14:35:49 +0900

    Feature/book service (#17) # Conflicts: #	deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/mapper/BookMapper.java

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/mapper/BookMapper.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/mapper/BookMapper.java
@@ -1,25 +1,24 @@
 package com.deokhugam.deokhugam_server.domain.book.mapper;
 
-import com.deokhugam.deokhugam_server.domain.book.entity.PopularBook;
-import com.deokhugam.deokhugam_server.domain.book.dto.response.PopularBookDto;
+import com.deokhugam.deokhugam_server.domain.book.dto.response.BookDto;
+import com.deokhugam.deokhugam_server.domain.book.entity.Book;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookMapper {
 
-  public static PopularBookDto toPopularDto(PopularBook entity) {
-    return new PopularBookDto(
-        entity.getId(),
-        entity.getBook().getId(),
-        entity.getBook().getTitle(),
-        entity.getBook().getAuthor(),
-        entity.getBook().getImageUrl(),
-        entity.getPeriodType(),
-        entity.getRankOrder(),
-        entity.getScore(),
-        entity.getReviewCount().intValue(),
-        entity.getRating(),
-        entity.getCreatedAt()
-    );
-  }
+    public BookDto toDto(Book book) {
+        return new BookDto(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getIsbn(),
+                book.getPublisher(),
+                book.getDescription(),
+                book.getImageUrl(),
+                book.getPublishedAt(),
+                book.getCreatedAt(),
+                book.getUpdatedAt()
+        );
+    }
 }
