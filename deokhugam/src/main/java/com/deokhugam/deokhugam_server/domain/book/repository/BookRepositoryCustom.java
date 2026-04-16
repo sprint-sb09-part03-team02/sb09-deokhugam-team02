1df453dbd09ca790d99dbfe715f14ab2e9b69cae
jieun <clover6559@gmail.com>
2026-04-16 11:17:44 +0900

    feat: 대시보드 관련 repository 구현

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/repository/BookRepositoryCustom.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/repository/BookRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.deokhugam.deokhugam_server.domain.book.repository;
 import com.deokhugam.deokhugam_server.domain.book.dto.request.BookSearchRequest;
 import com.deokhugam.deokhugam_server.domain.book.dto.response.BookRankQueryDto;
 import com.deokhugam.deokhugam_server.domain.book.entity.Book;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Slice;
@@ -16,5 +17,5 @@ public interface BookRepositoryCustom {
             LocalDateTime endDate,
             int limit
     );
-    List<BookRankQueryDto> findBookStatisticsForRanking(LocalDateTime startDate, LocalDateTime endDate);
+    List<BookRankQueryDto> findBookStatisticsForRanking(LocalDate startDate, LocalDate endDate);
 }
