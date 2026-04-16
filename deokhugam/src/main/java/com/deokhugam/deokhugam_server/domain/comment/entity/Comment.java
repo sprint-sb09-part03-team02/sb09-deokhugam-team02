7c1fa28b9ddf40ff6f58f23eabfe4ef3db7af8ad
구민주 <mj55991188@gmail.com>
2026-04-16 11:37:26 +0900

    refactor: Comment 엔티티를 일반 @Builder 방식으로 원복

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/comment/entity/Comment.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/comment/entity/Comment.java
@@ -1,23 +1,17 @@
 package com.deokhugam.deokhugam_server.domain.comment.entity;
 
 import com.deokhugam.deokhugam_server.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import java.util.UUID;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder; // @SuperBuilder 임포트
 
 @Entity
 @Table(name = "comments")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SuperBuilder
 public class Comment extends BaseEntity {
 
   @Id
@@ -36,11 +30,18 @@ public class Comment extends BaseEntity {
   @Column(nullable = false)
   private boolean isDeleted = false;
 
+  @Builder
+  public Comment(UUID reviewId, UUID userId, String content) {
+    this.reviewId = reviewId;
+    this.userId = userId;
+    this.content = content;
+  }
+
   public void updateContent(String content) {
     this.content = content;
   }
 
-  public void delete() { // 논리 삭제 원칙 준수
+  public void delete() {
     this.isDeleted = true;
   }
 }
