671489351fa29bc142df79eb55aee4ec899725e6
sungho <parksh3070@daum.net>
2026-04-20 17:25:03 +0900

    feat: import문 추가
    bookserviceimpl 에서 import 추가

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/service/BookServiceImpl.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/book/service/BookServiceImpl.java
@@ -20,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
