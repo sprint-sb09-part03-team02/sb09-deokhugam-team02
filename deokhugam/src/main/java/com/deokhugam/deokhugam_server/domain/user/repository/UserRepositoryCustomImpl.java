8450538923941b247c7b4e78fcb69d7edddc3109
jieun <clover6559@gmail.com>
2026-04-23 15:14:54 +0900

    refactor: userRepository 조건 변경

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/user/repository/UserRepositoryCustomImpl.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/user/repository/UserRepositoryCustomImpl.java
@@ -9,30 +9,43 @@ import com.deokhugam.deokhugam_server.domain.user.dto.response.UserRankQueryDto;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+
   private final JPAQueryFactory queryFactory;
 
   @Override
   public List<UserRankQueryDto> findUserActivityStatistics(LocalDate start, LocalDate end) {
+    LocalDateTime startDt = start.atStartOfDay();
+    LocalDateTime endDt = end.atTime(LocalTime.MAX);
 
     return queryFactory
-        .select(Projections.constructor(UserRankQueryDto.class,
-            user.id,
-            review.id.countDistinct(),
-            reviewLike.id.countDistinct(),
-            comment.id.countDistinct()
-        ))
-        .from(user)
-        .leftJoin(review).on(review.user.id.eq(user.id))
-        .leftJoin(reviewLike).on(reviewLike.user.id.eq(user.id))
-        .leftJoin(comment).on(comment.userId.eq(user.id))
-        .where(user.createdAt.between(start.atStartOfDay(), end.atTime(LocalTime.MAX)))
-        .groupBy(user.id)
-        .fetch();
+      .select(Projections.constructor(UserRankQueryDto.class,
+        user.id,
+        review.id.countDistinct(),
+        reviewLike.id.countDistinct(),
+        comment.id.countDistinct()
+      ))
+      .from(user)
+      .leftJoin(review).on(
+        review.user.id.eq(user.id),
+        review.createdAt.between(startDt, endDt)
+      )
+      .leftJoin(reviewLike).on(
+        reviewLike.user.id.eq(user.id),
+        reviewLike.createdAt.between(startDt, endDt)
+      )
+      .leftJoin(comment).on(
+        comment.userId.eq(user.id),
+        comment.createdAt.between(startDt, endDt)
+      )
+      .where(user.isDeleted.isFalse())
+      .groupBy(user.id)
+      .fetch();
   }
 }
