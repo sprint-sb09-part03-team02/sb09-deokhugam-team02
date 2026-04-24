2485cb2a3e16aff0da8f3ccfb6a14146ce0e5e09
jieun <clover6559@gmail.com>
2026-04-24 14:30:40 +0900

    feat: 로그인 성공 시 JWT 토큰 발급 및 응답 헤더 추가

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/user/controller/UserController.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import com.deokhugam.deokhugam_server.global.response.CursorPageResponse;
 import com.deokhugam.deokhugam_server.global.type.Period;
 import com.deokhugam.deokhugam_server.domain.user.dto.response.UserDto;
 import com.deokhugam.deokhugam_server.domain.user.service.UserService;
+import com.deokhugam.deokhugam_server.global.util.JwtProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
   private final UserService userService;
+  private final JwtProvider jwtProvider;
 
   @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
   @PostMapping
@@ -36,8 +38,10 @@ public class UserController {
   @PostMapping("/login")
   public ResponseEntity<UserDto> login(@Valid @RequestBody UserLoginRequest request) {
     UserDto userDto = userService.login(request);
+    String token = jwtProvider.generateToken(userDto.id());
     return ResponseEntity.status(HttpStatus.OK)
         .header("Deokhugam-Request-User-ID", userDto.id().toString())
+        .header("Authorization", "Bearer " + token)
         .body(userDto);
   }
 
