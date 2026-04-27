11e852b32a302de37bec0ccd29f5498a4f211ef7
jieun <clover6559@gmail.com>
2026-04-27 16:46:27 +0900

    feat: 데이터 기반 랭킹 배치 로직 추가

--- deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/review/batch/PopularReviewBatch.java
+++ deokhugam/src/main/java/com/deokhugam/deokhugam_server/domain/review/batch/PopularReviewBatch.java
@@ -3,6 +3,7 @@ package com.deokhugam.deokhugam_server.domain.review.batch;
 import com.deokhugam.deokhugam_server.domain.review.service.PopularReviewService;
 import com.deokhugam.deokhugam_server.global.type.Period;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Component;
 public class PopularReviewBatch {
   private final PopularReviewService popularReviewService;
 
+  @Scheduled(cron = "0 0 3 * * *")
+  public void executeDaily() {
+    execute(Period.DAILY);
+  }
   public void execute(Period period) {
     popularReviewService.calculateAndSaveReviewRanks(period);
   }
