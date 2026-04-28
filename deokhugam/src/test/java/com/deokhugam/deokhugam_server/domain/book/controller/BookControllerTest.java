8c9110bf36957b0ec91492b98a1c7593e1bfca0d
devseongjun <jseong013@gmail.com>
2026-04-28 16:39:19 +0900

    refactor(test): replace MockBean with MockitoBean in BookControllerTest

--- deokhugam/src/test/java/com/deokhugam/deokhugam_server/domain/book/controller/BookControllerTest.java
+++ deokhugam/src/test/java/com/deokhugam/deokhugam_server/domain/book/controller/BookControllerTest.java
@@ -28,8 +28,8 @@ import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(BookController.class)
@@ -39,7 +39,7 @@ class BookControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
+  @MockitoBean
   private BookService bookService;
 
   @Test
